332cd8476fe9c6e521e8a68c2054a74897b4f6fd
Matthew Baldwin <baldwinm@amazon.com>
2016-12-09 17:47:16 +0000

    Allow Splitting of SecurityGroups
    This commit changes it such that only one Ingress Port is enabled on a security group to prevent exceeding maximum ingress rules when adding new CloudFront ranges.  In order to allow both HTTP and HTTPS inbound from CloudFront you will need to create two security groups and tag them appropriately, with either Protocol: http or Protocol: https.

--- update_security_groups_lambda/update_security_groups.py
+++ update_security_groups_lambda/update_security_groups.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 
@@ -16,68 +16,76 @@ import urllib2
 # Name of the service, as seen in the ip-groups.json file, to extract information for
 SERVICE = "CLOUDFRONT"
 # Ports your application uses that need inbound permissions from the service for
-INGRESS_PORTS = [ 80, 443 ]
+INGRESS_PORTS = { 'Http' : 80, 'Https': 443 }
 # Tags which identify the security groups you want to update
-SECURITY_GROUP_TAGS = { 'Name': 'cloudfront', 'AutoUpdate': 'true' }
+SECURITY_GROUP_TAG_FOR_HTTP = { 'Name': 'cloudfront', 'AutoUpdate': 'true', 'Protocol': 'http' }
+SECURITY_GROUP_TAG_FOR_HTTPS = { 'Name': 'cloudfront', 'AutoUpdate': 'true', 'Protocol': 'https' }
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     message = json.loads(event['Records'][0]['Sns']['Message'])
-    
+
     # Load the ip ranges from the url
     ip_ranges = json.loads(get_ip_groups_json(message['url'], message['md5']))
-    
+
     # extract the service ranges
     cf_ranges = get_ranges_for_service(ip_ranges, SERVICE)
-    
+
     # update the security groups
     result = update_security_groups(cf_ranges)
-    
+
     return result
-    
+
 def get_ip_groups_json(url, expected_hash):
     print("Updating from " + url)
 
     response = urllib2.urlopen(url)
     ip_json = response.read()
-    
+
     m = hashlib.md5()
     m.update(ip_json)
     hash = m.hexdigest()
-    
+
     if hash != expected_hash:
         raise Exception('MD5 Mismatch: got ' + hash + ' expected ' + expected_hash)
-    
+
     return ip_json
-    
+
 def get_ranges_for_service(ranges, service):
     service_ranges = list()
     for prefix in ranges['prefixes']:
         if prefix['service'] == service:
             print('Found ' + service + ' range: ' + prefix['ip_prefix'])
             service_ranges.append(prefix['ip_prefix'])
-    
+
     return service_ranges
 
 def update_security_groups(new_ranges):
     client = boto3.client('ec2')
-    
-    groups = get_security_groups_for_update(client)
-    print ('Found ' + str(len(groups)) + ' SecurityGroups to update')
+
+    http_group = get_security_groups_for_update(client, SECURITY_GROUP_TAG_FOR_HTTP)
+    https_group = get_security_groups_for_update(client, SECURITY_GROUP_TAG_FOR_HTTPS)
+    print ('Found ' + str(len(http_group)) + ' HttpSecurityGroups to update')
+    print ('Found ' + str(len(https_group)) + ' HttpsSecurityGroups to update')
 
     result = list()
-    updated = 0
-    
-    for group in groups:
-        if update_security_group(client, group, new_ranges):
-            updated += 1
+    http_updated = 0
+    https_updated = 0
+    for group in http_group:
+        if update_security_group(client, group, new_ranges, INGRESS_PORTS['Http']):
+            http_updated += 1
             result.append('Updated ' + group['GroupId'])
-    
-    result.append('Updated ' + str(updated) + ' of ' + str(len(groups)) + ' SecurityGroups')
+    for group in https_group:
+        if update_security_group(client, group, new_ranges, INGRESS_PORTS[1]):
+            https_updated += 1
+            result.append('Updated ' + group['GroupId'])
+
+    result.append('Updated ' + str(http_updated) + ' of ' + str(len(http_group)) + ' HttpSecurityGroups')
+    result.append('Updated ' + str(https_updated) + ' of ' + str(len(https_group)) + ' HttpsSecurityGroups')
 
     return result
 
-def update_security_group(client, group, new_ranges):
+def update_security_group(client, group, new_ranges, port):
     added = 0
     removed = 0
 
@@ -93,22 +101,21 @@ def update_security_group(client, group, new_ranges):
                     if new_ranges.count(cidr) == 0:
                         to_revoke.append(range)
                         print(group['GroupId'] + ": Revoking " + cidr + ":" + str(permission['ToPort']))
-            
+
                 for range in new_ranges:
                     if old_prefixes.count(range) == 0:
                         to_add.append({ 'CidrIp': range })
                         print(group['GroupId'] + ": Adding " + range + ":" + str(permission['ToPort']))
-            
+
                 removed += revoke_permissions(client, group, permission, to_revoke)
                 added += add_permissions(client, group, permission, to_add)
-    else:        
-        for port in INGRESS_PORTS:
-            to_add = list()
-            for range in new_ranges:
-                to_add.append({ 'CidrIp': range })
-                print(group['GroupId'] + ": Adding " + range + ":" + str(port))
-            permission = { 'ToPort': port, 'FromPort': port, 'IpProtocol': 'tcp'}
-            added += add_permissions(client, group, permission, to_add)
+    else:
+        to_add = list()
+        for range in new_ranges:
+            to_add.append({ 'CidrIp': range })
+            print(group['GroupId'] + ": Adding " + range + ":" + str(port))
+        permission = { 'ToPort': port, 'FromPort': port, 'IpProtocol': 'tcp'}
+        added += add_permissions(client, group, permission, to_add)
 
     print (group['GroupId'] + ": Added " + str(added) + ", Revoked " + str(removed))
     return (added > 0 or removed > 0)
@@ -121,11 +128,11 @@ def revoke_permissions(client, group, permission, to_revoke):
             'IpRanges': to_revoke,
             'IpProtocol': permission['IpProtocol']
         }
-        
+
         client.revoke_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[revoke_params])
-        
+
     return len(to_revoke)
-    
+
 def add_permissions(client, group, permission, to_add):
     if len(to_add) > 0:
         add_params = {
@@ -134,14 +141,14 @@ def add_permissions(client, group, permission, to_add):
             'IpRanges': to_add,
             'IpProtocol': permission['IpProtocol']
         }
-        
+
         client.authorize_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[add_params])
-        
+
     return len(to_add)
-    
-def get_security_groups_for_update(client):
+
+def get_security_groups_for_update(client, security_group_tag):
     filters = list();
-    for key, value in SECURITY_GROUP_TAGS.iteritems():
+    for key, value in security_group_tag.iteritems():
         filters.extend(
             [
                 { 'Name': "tag-key", 'Values': [ key ] },
@@ -150,11 +157,11 @@ def get_security_groups_for_update(client):
         )
 
     response = client.describe_security_groups(Filters=filters)
-    
+
     return response['SecurityGroups']
 
 '''
- Sample Event From SNS:
+Sample Event From SNS:
 
 {
   "Records": [
