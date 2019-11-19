f4867cfa2a1fe03316a06e6e57c960404a84e5e3
Rarylson Freitas <rarylson@gmail.com>
2019-11-18 23:47:17 -0300

    Minor code improvements
    For example, removing an unecessary `;` and adding some blank lines (to be
    PEP-8 compliant).

--- update_security_groups_lambda/update_security_groups.py
+++ update_security_groups_lambda/update_security_groups.py
@@ -16,6 +16,7 @@ import hashlib
 import json
 import urllib.request, urllib.error, urllib.parse
 
+
 # Name of the service, as seen in the ip-groups.json file, to extract information for
 SERVICE = "CLOUDFRONT"
 # Ports your application uses that need inbound permissions from the service for
@@ -26,25 +27,27 @@ SECURITY_GROUP_TAG_FOR_GLOBAL_HTTPS = { 'Name': 'cloudfront_g', 'AutoUpdate': 't
 SECURITY_GROUP_TAG_FOR_REGION_HTTP = { 'Name': 'cloudfront_r', 'AutoUpdate': 'true', 'Protocol': 'http' }
 SECURITY_GROUP_TAG_FOR_REGION_HTTPS = { 'Name': 'cloudfront_r', 'AutoUpdate': 'true', 'Protocol': 'https' }
 
+
 def lambda_handler(event, context):
-    print(("Received event: " + json.dumps(event, indent=2)))
+    print("Received event: " + json.dumps(event, indent=2))
     message = json.loads(event['Records'][0]['Sns']['Message'])
 
     # Load the ip ranges from the url
     ip_ranges = json.loads(get_ip_groups_json(message['url'], message['md5']))
 
-    # extract the service ranges
+    # Extract the service ranges
     global_cf_ranges = get_ranges_for_service(ip_ranges, SERVICE, "GLOBAL")
     region_cf_ranges = get_ranges_for_service(ip_ranges, SERVICE, "REGION")
     ip_ranges = { "GLOBAL": global_cf_ranges, "REGION": region_cf_ranges }
 
-    # update the security groups
+    # Update the security groups
     result = update_security_groups(ip_ranges)
 
     return result
 
+
 def get_ip_groups_json(url, expected_hash):
-    print(("Updating from " + url))
+    print("Updating from " + url)
 
     response = urllib.request.urlopen(url)
     ip_json = response.read()
@@ -58,14 +61,16 @@ def get_ip_groups_json(url, expected_hash):
 
     return ip_json
 
+
 def get_ranges_for_service(ranges, service, subset):
     service_ranges = list()
     for prefix in ranges['prefixes']:
         if prefix['service'] == service and ((subset == prefix['region'] and subset == "GLOBAL") or (subset != 'GLOBAL' and prefix['region'] != 'GLOBAL')):
-            print(('Found ' + service + ' region: ' + prefix['region'] + ' range: ' + prefix['ip_prefix']))
+            print('Found ' + service + ' region: ' + prefix['region'] + ' range: ' + prefix['ip_prefix'])
             service_ranges.append(prefix['ip_prefix'])
 
     return service_ranges
+
 
 def update_security_groups(new_ranges):
     client = boto3.client('ec2')
@@ -75,10 +80,10 @@ def update_security_groups(new_ranges):
     region_http_group = get_security_groups_for_update(client, SECURITY_GROUP_TAG_FOR_REGION_HTTP)
     region_https_group = get_security_groups_for_update(client, SECURITY_GROUP_TAG_FOR_REGION_HTTPS)
 
-    print(('Found ' + str(len(global_http_group)) + ' CloudFront_g HttpSecurityGroups to update'))
-    print(('Found ' + str(len(global_https_group)) + ' CloudFront_g HttpsSecurityGroups to update'))
-    print(('Found ' + str(len(region_http_group)) + ' CloudFront_r HttpSecurityGroups to update'))
-    print(('Found ' + str(len(region_https_group)) + ' CloudFront_r HttpsSecurityGroups to update'))
+    print('Found ' + str(len(global_http_group)) + ' CloudFront_g HttpSecurityGroups to update')
+    print('Found ' + str(len(global_https_group)) + ' CloudFront_g HttpsSecurityGroups to update')
+    print('Found ' + str(len(region_http_group)) + ' CloudFront_r HttpSecurityGroups to update')
+    print('Found ' + str(len(region_https_group)) + ' CloudFront_r HttpsSecurityGroups to update')
 
     result = list()
     global_http_updated = 0
@@ -110,6 +115,7 @@ def update_security_groups(new_ranges):
 
     return result
 
+
 def update_security_group(client, group, new_ranges, port):
     added = 0
     removed = 0
@@ -125,12 +131,12 @@ def update_security_group(client, group, new_ranges, port):
                     old_prefixes.append(cidr)
                     if new_ranges.count(cidr) == 0:
                         to_revoke.append(range)
-                        print((group['GroupId'] + ": Revoking " + cidr + ":" + str(permission['ToPort'])))
+                        print(group['GroupId'] + ": Revoking " + cidr + ":" + str(permission['ToPort']))
 
                 for range in new_ranges:
                     if old_prefixes.count(range) == 0:
                         to_add.append({ 'CidrIp': range })
-                        print((group['GroupId'] + ": Adding " + range + ":" + str(permission['ToPort'])))
+                        print(group['GroupId'] + ": Adding " + range + ":" + str(permission['ToPort']))
 
                 removed += revoke_permissions(client, group, permission, to_revoke)
                 added += add_permissions(client, group, permission, to_add)
@@ -138,12 +144,13 @@ def update_security_group(client, group, new_ranges, port):
         to_add = list()
         for range in new_ranges:
             to_add.append({ 'CidrIp': range })
-            print((group['GroupId'] + ": Adding " + range + ":" + str(port)))
+            print(group['GroupId'] + ": Adding " + range + ":" + str(port))
         permission = { 'ToPort': port, 'FromPort': port, 'IpProtocol': 'tcp'}
         added += add_permissions(client, group, permission, to_add)
 
-    print((group['GroupId'] + ": Added " + str(added) + ", Revoked " + str(removed)))
+    print(group['GroupId'] + ": Added " + str(added) + ", Revoked " + str(removed))
     return (added > 0 or removed > 0)
+
 
 def revoke_permissions(client, group, permission, to_revoke):
     if len(to_revoke) > 0:
@@ -158,6 +165,7 @@ def revoke_permissions(client, group, permission, to_revoke):
 
     return len(to_revoke)
 
+
 def add_permissions(client, group, permission, to_add):
     if len(to_add) > 0:
         add_params = {
@@ -171,8 +179,9 @@ def add_permissions(client, group, permission, to_add):
 
     return len(to_add)
 
+
 def get_security_groups_for_update(client, security_group_tag):
-    filters = list();
+    filters = list()
     for key, value in security_group_tag.items():
         filters.extend(
             [
@@ -184,6 +193,7 @@ def get_security_groups_for_update(client, security_group_tag):
     response = client.describe_security_groups(Filters=filters)
 
     return response['SecurityGroups']
+
 
 '''
 Sample Event From SNS:
